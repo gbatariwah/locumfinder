c59fc30f0e994850d38830e52b7978a24101d97c
gbatariwah <batariwahg@gmail.com>
2022-02-23 17:59:51 +0000

    customization of jobs and comments admins (search_fields, ordering, list_filter, list_display and CommentsInline

--- jobs/admin.py
+++ jobs/admin.py
@@ -2,13 +2,24 @@ from django.contrib import admin
 from .models import Job, Comment
 
 
-# Register your models here.
+class CommentsInline(admin.StackedInline):
+    model = Comment
+    extra = 0
+
+
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
-    pass
+    inlines = [CommentsInline]
+    search_fields = ['title', 'description']
+    ordering = ['created_at']
+    list_filter = ['type', 'created_at', 'region', 'posted_by']
+    list_display = ['title', 'type', 'region', 'posted_by']
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['comment']
+    ordering = ['created_at']
+    list_filter = ['created_at', 'user']
+    list_display = ['message', 'job', 'user']
 
